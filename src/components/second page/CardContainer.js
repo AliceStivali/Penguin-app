bbd63667ea0c85ea08c61831866c9a8ae9a9d4c5
Alice Stivali <alicestivali@MacBook-Pro-di-Alice.local>
2022-10-06 14:10:19 +0200

    added props children

--- src/components/second page/CardContainer.js
+++ src/components/second page/CardContainer.js
@@ -1,8 +1,11 @@
 import { Card } from "./Card";
+import { Weather } from "./Weather";
 
 export function CardContainer() {
     return <div className="flex flex-nowrap gap-x-7 justify-center items-center">
-        <Card />
+        <Card>
+            <Weather />
+        </Card>
         <Card />
         <Card />
     </div>
