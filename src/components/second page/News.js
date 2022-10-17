f075fe10e5319de1572cc3f89ebc9ba07eccc398
Francesca Foglietta <francesfogli@gmail.com>
2022-10-17 23:24:26 +0200

    add div for each  articles

--- src/components/second page/News.js
+++ src/components/second page/News.js
@@ -1,27 +1,41 @@
 import { useFetchNews } from "./useFetchNews";
+import { useState } from "react";
 
 
 export function News() {
+    const { data, error, loading, articlesNumber } = useFetchNews()
+     const [i, setI] = useState(0)
 
-    const { data, error, loading } = useFetchNews()
+    function changeIndex() {
+        if (i + 2 <= articlesNumber) {
+          setI(i+ 2);
+        }
+      }
+
     console.log(data)
+    console.log(articlesNumber)
+    console.log(i)
     return (
-        <div className="flex-col justify-center items-space-between; news-list-style">
+        <div className="flex-col flex-wrap justify-center items-space-between; news-list-style">
             {loading && <h2>Caricamento...</h2>}
             {error && <h2>C'è stato un errore</h2>}
 
-            {data && (
-                <div className="news-container">
-                    <div>
-                        {/* {data.articles.map((item, index) => <li key={(index)}>{item.title}</li>)} */}
-                    </div>
-                </div> 
-            )} 
-            {data && (
-                <div className="news-container">
-
-                </div>
-            )}
+            {data && 
+                        <div> 
+                        <div className="news-container" key={(data.articles[i].title)}>
+                            <p style={{color:'black', paddingLeft:'0.7rem', paddingTop:'1rem'}}>{data.articles[i].title}</p>
+                        </div>
+             </div> }    
+             {data && i + 1 < articlesNumber &&
+                        <div> 
+                        <div className="news-container" key={(data.articles[i].title)}>
+                            <p style={{color:'black', paddingLeft:'0.7rem', paddingTop:'1rem'}}>{data.articles[i + 1].title}</p>
+                        </div>
+             </div> }          
+      
+            <div>
+                <button onClick={changeIndex}> switch </button>
+            </div>
         </div>
     )
 }
--- src/components/second page/useFetchNews.js
+++ src/components/second page/useFetchNews.js
@@ -13,13 +13,26 @@ const fetcher = (url) =>
   fetch(url, options).then((response) => response.json());
 
 export function useFetchNews() {
-  const { data, error } = useSWR( `https://newscatcher.p.rapidapi.com/v1/search_enterprise?q=${city}&lang=en&sort_by=date&search_in=title&topic=entertainment&page=1&media=True`
+  const { data, error } = useSWR(`https://newscatcher.p.rapidapi.com/v1/search_enterprise?q=
+  ${city}&lang=en&sort_by=date&search_in=title&topic=entertainment&page=1&media=True`
   ,fetcher);
+
+  let articlesNumber = 0
+  if (data) {
+  if(data.total_hits >= 50) {
+      articlesNumber = 50
+  } else {
+      articlesNumber = data.total_hits
+  }
+} 
+
 
   return (
     {
     data: data,
     error: error,
-    loading: !data && !error}
+    loading: !data && !error,
+    articlesNumber : articlesNumber}
+    
   )
 }
