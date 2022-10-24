5fe6779ef75adc110b0103eda2e2f1948c420907
Alice Stivali <alicestivali@MacBook-Pro-di-Alice.local>
2022-10-24 10:37:42 +0200

    added bootstrap

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import ReactDOM from "react-dom";
-import './style/index.css'
-import './style/output.css'
+import "./style/index.css";
+import "./style/output.css";
 import { App } from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const root = document.querySelector('#root')
+const root = document.querySelector("#root");
 
-ReactDOM.render(<App />, root)
+ReactDOM.render(<App />, root);
