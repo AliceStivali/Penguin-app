3264d7fe944ea7be2badd704473c6c5760f40852
frances8713 <104068962+frances8713@users.noreply.github.com>
2022-10-27 23:24:41 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import InputAirport from "./components/first page/InputAirport";
 import { Home } from "./components/first page/Home";
-import { SecondPageHome } from "./components/second page/SecondPageHome";
+import { HomeSearch } from "./components/second page/HomeSearch";
 
 export function App() {
   return (
     <div>
     {/* <Home /> */}
-    {/* <SecondPageHome /> */}
+    {/* <HomeSearch /> */}
       <InputAirport />
     </div>
   );
