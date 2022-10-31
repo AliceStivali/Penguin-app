259fe3b671ca7db05759dab1c690d2cb655e20f2
Alice Stivali <alicestivali@MacBook-Pro-di-Alice.local>
2022-10-31 16:06:01 +0100

    added animations for buttons hover

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { FlightsWithReturn } from "./components/second page/FlightsWithReturn";
+import { Search } from "./components/first page/Search";
 
 export function App() {
   return (
     <div>
-      <FlightsWithReturn />
+      <Search />
     </div>
   );
 }
